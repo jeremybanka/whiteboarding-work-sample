22c5de9edf38251970186add25f3444b5b4bfa77
Jeremy Banka <hello@jeremybanka.com>
2021-06-25 11:00:57 -0700

    ✨ add convenient node  tool

---
+++ log.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const solution1 = require(`./1.js`)
+const solution2 = require(`./2.js`)
+
+const run = (chosenFile: number, input: string):void => {
+  switch (chosenFile) {
+    case 1: return solution1(input)
+    case 2: return solution2(input)
+    default: return console.log(
+      `invalid file name`,
+      `valid options`,
+      `1, 2, 3, 4, 5`
+    )
+  }
+}
+
+const chosenFile = parseInt(process.argv[2], 10)
+const input = process.argv[3]
+run(chosenFile, input)
